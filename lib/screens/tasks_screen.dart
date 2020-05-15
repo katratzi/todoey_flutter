0926d9d922a55daf3e56d6798ee1a33c7d78a4cd
katratzi <ric_clifford@hotmail.com>
2020-05-15 12:54:30 +0100

    bottom card and floating button added in

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,40 +5,66 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlue,
-      body: Container(
-        padding: EdgeInsets.fromLTRB(20.0, 60.0, 20.0, 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 40.0,
-              child: Icon(
-                Icons.list,
-                color: Colors.lightBlue,
-                size: 40.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'Todoey',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 40.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Text(
-              '12 Tasks',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18.0,
-              ),
-            )
-          ],
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlue,
+        onPressed: () {
+          print('fab pressed');
+        },
+        child: Icon(
+          Icons.add,
         ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(20.0, 60.0, 20.0, 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 40.0,
+                  child: Icon(
+                    Icons.list,
+                    color: Colors.lightBlue,
+                    size: 40.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
