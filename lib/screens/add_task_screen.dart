a74bec0c13eac4b032174479418edee009f9671c
katratzi <ric_clifford@hotmail.com>
2020-05-17 21:27:55 +0100

    layout for the bottom addtask screen

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -13,6 +13,39 @@ class AddTaskScreen extends StatelessWidget {
             topRight: Radius.circular(30.0),
           ),
         ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                'Add Task',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.lightBlueAccent,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              TextField(
+                // autofocus: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  print(value);
+                },
+              ),
+              FlatButton(
+                color: Colors.lightBlueAccent,
+                onPressed: () {
+                  print('Add task');
+                },
+                child: Text(
+                  'Add',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
